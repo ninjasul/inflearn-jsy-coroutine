6b7a7dfdc3c4e04ba3a29f1b14bea13d624ba952
dongyub.park <dongyub.park@42dot.ai>
2025-01-14 07:27:11 +0900

    commit Code2s

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // 코루틴 라이브러리
-    implementation("org.jetbrains.kotlin:kotlinx-coroutines-core:1.8.1")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 tasks.test {
---
+++ src/main/kotlin/section1/code2/Code2-5.kt
@@ -0,0 +1,24 @@
+package section1.code2
+
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
+
+fun main() {
+    val executorService = Executors.newFixedThreadPool(2)
+
+    // ExecutorService에 반환 값이 있는 작업 제출
+    val completableFuture = CompletableFuture.supplyAsync(
+        {
+            Thread.sleep(2000L) // 2초 대기
+            return@supplyAsync "더미 결과값"
+        },
+        executorService
+    )
+
+    // 콜백 형식으로 결과값 처리
+    completableFuture.thenAccept { result ->
+        println("결과: $result")
+    }
+
+    executorService.shutdown()
+}
