e71cc71e2a25f843df7e9db618bdd95f5725326c
dongyub.park <dongyub.park@42dot.ai>
2025-01-14 06:05:29 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "me.dypark"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // 코루틴 라이브러리
+    implementation("org.jetbrains.kotlin:kotlinx-coroutines-core:1.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
