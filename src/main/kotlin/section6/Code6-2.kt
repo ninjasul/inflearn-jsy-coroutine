6a5a3b70671b7ecb1a315b54ec833f09a5b5bb7c
dongyub.park <dongyub.park@42dot.ai>
2025-01-30 17:32:13 +0900

    6-2

---
+++ src/main/kotlin/section6/Code6-2.kt
@@ -0,0 +1,17 @@
+package section6
+
+import kotlinx.coroutines.*
+
+fun main() {
+    val newScope: CoroutineScope = CoroutineScope(
+        CoroutineName("MyCoroutine") + Dispatchers.IO
+    )
+
+    newScope.launch(context = CoroutineName("LaunchCoroutine")) {
+        println("newScope의 coroutineContext: ${newScope.coroutineContext}")
+        println("launch 코루틴의 coroutineContext: ${this.coroutineContext}")
+        println("launch 코루틴의 parentJob: ${this.coroutineContext[Job]?.parent}")
+    }
+
+    Thread.sleep(1000L)
+}
